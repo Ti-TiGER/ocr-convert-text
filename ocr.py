9066130ff0a28f8a012154df4976311e932de137
Ti-TiGER <75837562+Ti-TiGER@users.noreply.github.com>
2021-04-30 05:28:28 +0700

    intial commit

--- ocr.py
+++ ocr.py
@@ -4,21 +4,12 @@ import sys
 
 
 def read_image(img_path, lang='eng'):
-    """
-    Performs OCR on a single image
-
-    :img_path: str, path to the image file
-    :lang: str, language to be used while conversion (optional, default is english)
-
-    Returns
-    :text: str, converted text from image
-    """
-
     try:
         pytesseract.pytesseract.tesseract_cmd = 'E:\Tesseract-OCR\\tesseract'
         return pytesseract.image_to_string(img_path, lang=lang)
     except:
         return "[ERROR] Unable to process file: {0}".format(img_path)
+
 
 def _write_to_file(text, file_path):
     """
